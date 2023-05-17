6cc80cf0aa0ca9ba72ef7b447870c86e0e102cb7
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-05-17 22:44:29 +0600

    Initial commit.

---
+++ EmployeesMVVM/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  EmployeesMVVM
+//
+//  Created by Kuanysh al-Khattab Auyelgazy on 17.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
